5336b0a4fcf7efaacacee872ab9c9643b37ae075
Brice Folleas <brice.folleas@gmail.com>
2024-03-18 16:02:26 +0900

    add vuetify library

--- src/main.js
+++ src/main.js
@@ -7,13 +7,24 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import functions from './plugins/functions.js'
 import Particles from "vue3-particles";
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 library.add(fas)
 dom.watch();
 
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
 createApp(App)
     .use(store)
     .use(i18n)
+    .use(vuetify)
     .use(Particles)
     .component("font-awesome-icon", FontAwesomeIcon)
     .provide('$func', functions) // Providing to all components during app creation
