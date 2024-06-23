8250a0fa7540296f96d6392d5f0432772e4fcecf
Brice Folleas <brice.folleas@gmail.com>
2024-06-23 19:19:31 +0900

    fix: contact + try fixing skills icons

--- src/section/Contact.vue
+++ src/section/Contact.vue
@@ -1,91 +1,95 @@
 <template>
     <div class="flex-div contact-form">
-        <span id="contact" class="mb-60"></span>
-        <h2 class="h-center">{{ $t('contact.name') }}</h2>
-        <form @submit.prevent="submitForm">
-            <div class="form-group">
-                <label for="email">{{ $t('contact.sender') }}</label>
-                <input type="email" id="email" v-model="senderEmail" required>
-            </div>
-            <div class="form-group">
-                <label for="subject">{{ $t('contact.topic') }}</label>
-                <input type="text" id="subject" v-model="subject" required>
-            </div>
-            <div class="form-group">
-                <label for="content">{{ $t('contact.content') }}</label>
-                <textarea id="content" v-model="content" rows="4" required></textarea>
-            </div>
-            <button type="submit" class="action-btn">{{ $t('contact.send') }}</button>
-        </form>
+      <span id="contact" class="mb-60"></span>
+      <h2 class="h-center">{{ $t('contact.name') }}</h2>
+      <form @submit.prevent="sendMail">
+        <div class="form-group">
+          <label for="email">{{ $t('contact.sender') }}</label>
+          <input type="email" id="email" v-model="senderEmail" required />
+        </div>
+        <div class="form-group">
+          <label for="subject">{{ $t('contact.topic') }}</label>
+          <input type="text" id="subject" v-model="subject" required />
+        </div>
+        <div class="form-group">
+          <label for="content">{{ $t('contact.content') }}</label>
+          <textarea id="content" v-model="content" rows="4" required></textarea>
+        </div>
+        <button type="submit" class="action-btn">{{ $t('contact.send') }}</button>
+      </form>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: "ContactSection",
     data() {
-        return {
-            senderEmail: '',
-            subject: '',
-            content: '',
-            email: 'brice.folleas@gmail.com'
-        };
+      return {
+        senderEmail: "",
+        subject: "",
+        content: "",
+        email: "brice.folleas@gmail.com",
+      };
     },
     methods: {
-        sendMail() {
-            window.open("mailto:${this.email}?subject=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(this.content)}'", "_blank", "noopener");
-        }
-    }
-};
-</script>
-
-<style scoped>
-.contact-form {
+      sendMail() {
+        const mailtoLink = `mailto:${this.email}?subject=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(
+          `From: ${this.senderEmail}\n\n${this.content}`
+        )}`;
+        window.open(mailtoLink, "_blank", "noopener");
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .contact-form {
     margin: auto;
     padding: 20px;
-}
-
-form {
+  }
+  
+  form {
     width: 80%;
-}
-
-.action-btn {
+  }
+  
+  .action-btn {
     margin: 0;
-}
-
-.form-group {
+  }
+  
+  .form-group {
     margin-bottom: 20px;
-}
-
-label {
+  }
+  
+  label {
     display: block;
     margin-bottom: 5px;
-}
-
-input[type="email"],
-input[type="text"],
-textarea {
+  }
+  
+  input[type="email"],
+  input[type="text"],
+  textarea {
     width: 100%;
     padding: 10px;
     border: 2px solid var(--color-borders);
     border-radius: 1em;
     background-color: var(--color-background-soft);
     color: var(--color-text);
-}
-
-input[type="email"]:focus,
-input[type="text"]:focus,
-textarea:focus {
+  }
+  
+  input[type="email"]:focus,
+  input[type="text"]:focus,
+  textarea:focus {
     outline: none;
     border-color: var(--color-primary);
-}
-
-button {
+  }
+  
+  button {
     display: block;
     width: 100%;
-}
-
-.action-link {
+  }
+  
+  .action-link {
     color: var(--color-primary);
-}
-</style>
+  }
+  </style>
+  
